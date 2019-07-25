776c48b79ac1263732a1f4591325d2f7c26685f0
Mark Patton <mark.patton@gmail.com>
2019-07-25 12:06:13 -0400

    Fix typo in build script.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-schmeme="$1"
+scheme="$1"
 host="$2"
 port="$3"
-archive=$(realpath $4)
+archive=$(realpath data/archive)
 
 mvn clean install -Diiif.pres.scheme=$scheme -Diiif.pres.port=$port -Diiif.pres.host=$host -Diiif.pres.prefix=/rosademo/iiif -Dstatic.prefix=/rosademo/data -Diiif.image.scheme=https -Diiif.image.port=443 -Diiif.image.host=image.library.jhu.edu -Diiif.image.prefix=/iiif -Darchive.path=$archive

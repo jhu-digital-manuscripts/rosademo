99d669a76b51b54fa0435323c4a0e22160f97401
Mark Patton <mark.patton@gmail.com>
2019-07-25 15:00:23 -0400

    Add usage info

--- build.sh
+++ build.sh
@@ -5,4 +5,10 @@ host="$2"
 port="$3"
 archive=$(realpath data/archive)
 
+if [ $# -ne 3 ]; 
+   then
+       echo "Expected ./build.sh scheme host port"
+       exit 1
+fi
+
 mvn clean install -Diiif.pres.scheme=$scheme -Diiif.pres.port=$port -Diiif.pres.host=$host -Diiif.pres.prefix=/rosademo/iiif -Dstatic.prefix=/rosademo/data -Diiif.image.scheme=https -Diiif.image.port=443 -Diiif.image.host=image.library.jhu.edu -Diiif.image.prefix=/iiif -Darchive.path=$archive

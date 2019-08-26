4be0ec5f59730bb521859082dfa4ff72429b02c6
Mark Patton <mark.patton@gmail.com>
2019-08-26 10:06:19 -0400

    Check JSON-LD expansion of Web Annotations

--- rosa-iiif/rosa-iiif-presentation-core/src/test/java/rosa/iiif/presentation/core/WebAnnotationServiceTest.java
+++ rosa-iiif/rosa-iiif-presentation-core/src/test/java/rosa/iiif/presentation/core/WebAnnotationServiceTest.java
@@ -1,15 +1,22 @@
 package rosa.iiif.presentation.core;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.utils.JsonUtils;
 
 import rosa.archive.core.BaseArchiveTest;
 import rosa.iiif.presentation.model.PresentationRequest;
@@ -27,26 +34,70 @@ public class WebAnnotationServiceTest extends BaseArchiveTest {
 
         service = new WebAnnotationService(cache);
     }
+
+    // Check that JSON-LD can be processed
+    private void validate_json_ld(InputStream is) throws Exception {
+        Object o = JsonUtils.fromInputStream(is);
+
+        List<Object> result = JsonLdProcessor.expand(o);
+        assertEquals(1, result.size());
+
+        String expanded = JsonUtils.toPrettyString(result.get(0));
+
+        check_exanded_json_ld(new JSONObject(expanded));
+    }
+
+    // Check that @id and @type keys have expanded values
+    private void check_exanded_json_ld(JSONObject o) {        
+        if (o == null) {
+            return;
+        }
         
+        JSONArray keys = o.names();
+        
+        if (keys == null) {
+            return;
+        }
+
+        for (int i = 0; i < keys.length(); ++i) {
+            String key = keys.getString(i); 
+            Object value = o.get(key);
+
+            if (value instanceof JSONObject) {
+               check_exanded_json_ld((JSONObject) value);
+            } else if (value instanceof JSONArray) {
+                JSONArray array = (JSONArray) value;
+                
+                for (int j = 0; j < array.length(); j++) {
+                    check_exanded_json_ld(array.optJSONObject(j));
+                }
+            } else if (value instanceof String) {                
+                if (key.equals("@id") || key.equals("@type")) {
+                    String s = (String) value;
+                    
+                    assertTrue(s.startsWith("http") || s.startsWith("urn:cts:"));
+                }
+            }
+        }
+    }
+
     @Test
-    public void testLudwigXV7ManifestRequest() throws IOException {
-        PresentationRequest req = new PresentationRequest(PresentationRequestType.CANVAS, VALID_COLLECTION, VALID_BOOK_LUDWIGXV7, "1r");
-        
+    public void testLudwigXV7ManifestRequest() throws Exception {
+        PresentationRequest req = new PresentationRequest(PresentationRequestType.CANVAS, VALID_COLLECTION,
+                VALID_BOOK_LUDWIGXV7, "1r");
+
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         assertTrue(service.handle_request("http://example.com/wa/col/book/1r/canvas", req, os));
-     
-        String result = new String(os.toByteArray(), "UTF-8");
-        System.err.println(result);
-        JSONObject json = new JSONObject(result);
-        
-        assertNotNull(json);
+
+        validate_json_ld(new ByteArrayInputStream(os.toByteArray()));
     }
-    
+
     @Test
     public void testUnknownCanvasRequest() throws IOException {
-        PresentationRequest req = new PresentationRequest(PresentationRequestType.CANVAS, VALID_COLLECTION, VALID_BOOK_FOLGERSHA2, "foo");
-        
+        PresentationRequest req = new PresentationRequest(PresentationRequestType.CANVAS, VALID_COLLECTION,
+                VALID_BOOK_FOLGERSHA2, "foo");
+
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         assertFalse(service.handle_request("http://example.com/wa/col/book/1r/canvas", req, os));
-    } 
+    }
 }
